bb398b3cc12f640abaec874656c7ba6d9ea471ee
schismatiic <leonardojofre2003@gmail.com>
2026-05-05 12:26:52 -0400

    fibs can return a fibonacci sequence

--- fibonacci/fibs.js
+++ fibonacci/fibs.js
@@ -1,1 +1,12 @@
-const fibs = (num) => {};
+const fibs = (num) => {
+  const fibArr = [];
+  for (let index = 0; index < num; index++) {
+    if (index === 0 || index === 1) {
+      fibArr[index] = index === 0 ? 0 : 1;
+    } else {
+      fibArr[index] = fibArr[index - 1] + fibArr[index - 2];
+    }
+  }
+  return fibArr;
+};
+console.log(fibs(8));
