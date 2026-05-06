0b14b76c89da96ea0a2287014e0694c6e753ede1
schismatiic <leonardojofre2003@gmail.com>
2026-05-05 21:53:40 -0400

    [fibRec]: the function works

--- fibonacci/fibsRec.js
+++ fibonacci/fibsRec.js
@@ -1,2 +1,25 @@
-const fibsRec = (num) => {};
+const fibsRec = (num) => {
+  debugger;
+  let currentFib = [];
+  if (num < 0 || Number.isInteger(num) === false) {
+    return undefined;
+  }
+  if (num === 0 || num === 1) {
+    return num === 0 ? [] : [0];
+  }
+  console.log("This was printed recursively");
+  const previousFib = fibsRec(num - 1);
+
+  previousFib.forEach((element, index) => {
+    currentFib = [
+      ...previousFib,
+      Number.isInteger(previousFib[index - 1]) === false
+        ? element + 1
+        : element + previousFib[index - 1],
+    ];
+  });
+  return currentFib;
+};
+console.log(fibsRec(4));
+
 module.exports = fibsRec;
