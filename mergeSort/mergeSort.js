3da6528b8095b43e0a041849d0d354453e115bc7
schismatiic <leonardojofre2003@gmail.com>
2026-05-07 12:18:49 -0400

    [mergeSort]: function works and all tests passed

--- mergeSort/mergeSort.js
+++ mergeSort/mergeSort.js
@@ -1,2 +1,31 @@
-const mergeSort = (arr) => {};
+const getHalf = (arr) => {
+  if (arr.length % 2 === 0) {
+    return arr.length / 2;
+  } else {
+    return (arr.length - 1) / 2;
+  }
+};
+const merge = (leftHalf, rightHalf) => {
+  const mergedArray = leftHalf.concat(rightHalf);
+  mergedArray.sort((a, b) => {
+    return a - b;
+  });
+  return mergedArray;
+};
+const mergeSort = (arr) => {
+  if (Array.isArray(arr) === false) {
+    return undefined;
+  }
+  if (arr.length === 0) {
+    return [];
+  }
+  if (arr.length === 1) {
+    return arr;
+  }
+  const half = getHalf(arr);
+  const leftHalf = arr.filter((element, index) => index < half);
+  const rightHalf = arr.filter((element, index) => index >= half);
+  return merge(mergeSort(leftHalf), mergeSort(rightHalf));
+};
+
 module.exports = mergeSort;
